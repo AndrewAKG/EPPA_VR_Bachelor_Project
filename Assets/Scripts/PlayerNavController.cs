6073a080c72d9fa8ea82a7b8fff6f30dbabe0258
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-06-27 16:14:34 +0200

    feat: Added Scripts, Animation, Prefabs and Scenes Folders

---
+++ Assets/Scripts/PlayerNavController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNavController : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Vertical");
+        anim.SetFloat("Speed", move);
+    }
+}
