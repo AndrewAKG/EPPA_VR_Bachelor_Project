9eedb2bba18c98f69ae615600e26adc59bcd4d00
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-07-04 17:08:04 +0200

    fix: Updated some animations

--- Assets/Scripts/PlayerNavController.cs
+++ Assets/Scripts/PlayerNavController.cs
@@ -7,17 +7,32 @@ public class PlayerNavController : MonoBehaviour {
     private Animator anim;
     public bool isWalking = false;
     public float speed;
+    public Vector3 originalPos;
+    public bool once = false;
 
    
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        originalPos = transform.position;
     }
 
     void Update()
     {
-    
+        if (!once)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerGoToSM"))
+            {
+                Debug.Log("Entered");
+            }
+            else
+            {
+                transform.position = originalPos;
+                once = true;
+            }
+        }
+        
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         transform.Rotate(0, x, 0);
 
@@ -29,7 +44,7 @@ public class PlayerNavController : MonoBehaviour {
                 anim.Play("M_walk");
             }
             var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-            transform.Translate(0, 0, z); // Only move when "w" is pressed.
+            transform.Translate(0, 0, z); // Only move when "up arrow" is pressed.
         }
         else
         {
