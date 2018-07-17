b63a39d841b5194751e39657530a4e6808b6fed1
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-07-17 15:36:36 +0200

    feat: added VRTK package

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform agent;
+    public Animator agentAnim;
+    public Transform player;
+    public AgentController controller;
+    //private bool once = false; 
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    public void setTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    private void Start()
+    {
+        //if (agent != null)
+        //    setTarget(agent);
+    }
+
+    IEnumerator Delay()
+    {
+        Debug.Log("1");
+        bool x = true;
+        while (x)
+        {
+            controller.getAgent().isStopped = true;
+            agentAnim.Play("M_idle1");
+            Debug.Log("2");
+            yield return new WaitForSeconds(2.0f);
+            Debug.Log("3");
+            agentAnim.Play("M_turnR90");
+            Debug.Log("4");
+            agentAnim.Play("M_turnR90");
+            Debug.Log("5");
+            yield return new WaitForSeconds(2.0f);
+            Debug.Log("6");
+            agentAnim.Play("M_clap");
+            Debug.Log("7");
+            yield return new WaitForSeconds(2.0f);
+            Debug.Log("8");
+            x = false;
+        }
+    }
+
+    //private void Update()
+    //{
+    //    if (!once)
+    //    {
+    //        if (controller != null)
+    //        {
+    //            if (controller.isFinished())
+    //            {
+    //                if (player != null)
+    //                {
+    //                    //StartCoroutine(Delay());
+    //                    setTarget(player);
+    //                    once = true;
+    //                }
+
+    //                agent.transform.localScale = new Vector3(0, 0, 0);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Debug.LogError("Controller Null");
+    //        }
+    //    }
+    //}
+        
+
+    private void LateUpdate()
+    { 
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/MaterialController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour {
+
+    Material m_Material;
+    Material m_red;
+    Material m_yellow;
+    Material m_green;
+    Renderer rend;
+    Renderer rend_m;
+    GameObject m;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        m_red = Resources.Load("TL_red") as Material;
+        m_yellow = Resources.Load("TL_yellow") as Material;
+        m_green = Resources.Load("TL_green") as Material;
+        m = GameObject.FindGameObjectWithTag("Moshah");
+        rend_m = m.GetComponent<Renderer>();
+        //Fetch the Material from the Renderer of the GameObject
+        //m_Material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print(rend.material.name);
+            rend.material = m_red;
+            rend_m.material = m_green;
+        }
+    }
+}
