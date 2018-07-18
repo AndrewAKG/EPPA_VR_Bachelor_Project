a4f20cf6c19e4b756b4cc801568e543ff10c5e85
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-07-18 17:11:11 +0200

    feat: added CanvasController script

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+
+    private GameObject agentCanvas;
+    private GameObject agent;
+    private bool showAgentCanvas = false;
+
+	// Use this for initialization
+	void Start () {
+        agentCanvas = GameObject.FindGameObjectWithTag("AgentCanvas");
+        agent = GameObject.FindGameObjectWithTag("Agent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            showAgentCanvas = !showAgentCanvas;
+        }
+
+        if (showAgentCanvas)
+        {
+            agentCanvas.GetComponent<CanvasGroup>().alpha = 1f;
+            agent.transform.localScale = Vector3.one;
+        }
+        else
+        {
+            agentCanvas.GetComponent<CanvasGroup>().alpha = 0f;
+            agent.transform.localScale = Vector3.zero;
+        }
+		
+	}
+}
