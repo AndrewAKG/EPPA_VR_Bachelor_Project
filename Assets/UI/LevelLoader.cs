9d2905496725835f28be3aeb39a87740ea1e3a37
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-07-29 23:19:26 +0200

    feat: added StartScene and loading screen

---
+++ Assets/UI/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+	public void LoadSimulatorScene(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            slider.value = operation.progress;
+            progressText.text = operation.progress * 100 + "%";
+            yield return null;
+        }
+    }
+}
