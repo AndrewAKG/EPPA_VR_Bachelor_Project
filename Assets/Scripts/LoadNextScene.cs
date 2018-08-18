b6441bb24b618cb2575dc251a23b6386e1f85b10
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-08-18 18:25:11 +0200

    feat: added main scene end collider

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
